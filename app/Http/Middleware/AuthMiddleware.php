17b18654085ab23739c778fb766b82d342098da5
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-01-27 19:04:54 -0400

    Add response for timezone in AuthMiddleware

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -24,6 +24,7 @@ class AuthMiddleware
             if(!$this->isValidTimeZone($timezone)){
                 $timezone = 'America/Caracas';
             }
+            return response($timezone);
             $user = JWTAuth::parseToken()->authenticate();
             if (!$user) throw new Exception();
         } catch (Exception $e) {
