3f436d7d0b5ddb5e841257eada139c6988e3d876
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-05-09 10:58:01 -0400

    Fix lazy loading of subreports and inconsistences data in ReportController

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -92,9 +92,16 @@ class ReportController extends Controller
         //If the user is admin and the request has a user id, get all reports from that user with subreports
         //else get the reports, with the subreport from current user
         if ($currentUser->role->id === 1) {
-            $query = $query->where('reports.user_id', $user)->with('type', 'subreports', 'user.role');
+            $query = $query->where('reports.user_id', $user)->with('type', 'subreports.inconsistences.data', 'user.role', )->paginate(10);
 
-            return response()->json($query->paginate(10), 200);
+            foreach ($query as $report) {
+                $report->subreports = $this->KeyMapValue->transformElement($report->subreports);
+                foreach ($report->subreports as $subreport) {
+                    $subreport->inconsistences = $this->KeyMapValue->transformElement($subreport->inconsistences);
+                }
+            }
+            
+            return response()->json($query, 200);
         } else {
             $query = $query->where('reports.user_id', $currentUser->id)->with('type', 'subreports');
 
@@ -150,13 +157,15 @@ class ReportController extends Controller
 
     public function show($id)
     {
-        $report = Report::with('type', 'user.role', 'subreports.data')->find($id);
+        $report = Report::with('type', 'user.role', 'subreports.data', 'subreports.inconsistences.data')->find($id);
         if (! $report) {
             return response()->json(['error' => 'No se encontró el reporte'], 404);
         }
         $currentUser = User::find(auth()->user()->id);
         $report->subreports = $this->KeyMapValue->transformElement($report->subreports);
-
+        foreach ($report->subreports as $subreport) {
+            $subreport->inconsistences = $this->KeyMapValue->transformElement($subreport->inconsistences);
+        }
         if ($currentUser->role->id === 1) {
             return response()->json($report, 200);
         } else {
