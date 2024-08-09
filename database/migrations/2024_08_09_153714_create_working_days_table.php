18d0303bbb865519b83c60dd327dcde369aef25a
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-08-09 11:38:42 -0400

    Fix working days table name

--- database/migrations/2024_08_09_153714_create_working_days_table.php
+++ database/migrations/2024_08_09_153714_create_working_days_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('working_day', function (Blueprint $table) {
+        Schema::create('working_days', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             //Store the working day in the format YYYY-MM-DD without time
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('working_day');
+        Schema::dropIfExists('working_days');
     }
 };
