320e88c32e7f320697447a3f195c4e44d5fe8b00
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-08-12 15:17:53 -0400

    feat: Format subreport creation date for working day

--- app/Models/WorkingDay.php
+++ app/Models/WorkingDay.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class WorkingDay extends Model
 {
@@ -18,8 +20,13 @@ class WorkingDay extends Model
     {
         return $this->belongsTo(User::class);
     }
-    public function didReport()
+    public function didReport(): HasMany
     {
-        return $this->hasOne(Subreport::class, 'created_at', 'date');
+        //return subreport where working day date is the same as the report created_at date
+        //but the created_at have the format YYYY-MM-DD HH:MM:SS and the date have the format YYYY-MM-DD
+        //so we need to format the created_at to YYYY-MM-DD
+
+        return $this->hasMany(Subreport::class, 'created_at', 'date')
+                    ->whereRaw('DATE(created_at) = ?', [Carbon::parse($this->date)->format('Y-m-d')]);
     }
 }
