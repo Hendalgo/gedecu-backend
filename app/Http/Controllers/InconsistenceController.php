8ff8561380cc3f559e183c53886428b01bebea66
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2024-10-04 23:10:45 -0400

    d

--- app/Http/Controllers/InconsistenceController.php
+++ app/Http/Controllers/InconsistenceController.php
@@ -492,7 +492,7 @@ class InconsistenceController extends Controller
 
             //Get siblings of the current subreport
             $subreports = $subreports->filter(function ($item) use ($sub, &$amount, &$transferences_quantity, $type) {
-                $subData = json_decode($sub->data, true);
+               /*  $subData = json_decode($sub->data, true);
                 if (gettype($item->data) == 'string') {
                     $itemData = json_decode($item->data, true);
                 } else {
@@ -512,7 +512,7 @@ class InconsistenceController extends Controller
                 if ($subData['rate'] == $itemData['rate'] && Carbon::parse($item->created_at)->diffInHours($sub->created_at) <= 24 && $sub->id != $item->id) {
                     $amount += $itemData['amount'];
                     $transferences_quantity += $itemData['transferences_quantity'];
-                }
+                } */
             });
 
             $amount += $subData['amount'];
@@ -522,7 +522,7 @@ class InconsistenceController extends Controller
 
             $filtered = $filtered->filter(function ($item) use ($sub, $type) {
                 //The bank account from the subreport should be the same as the bank account from the
-                $subData = json_decode($sub->data, true);
+                /* $subData = json_decode($sub->data, true);
                 $itemData = json_decode($item->data, true);
                 $store = $subData['store_id'];
                 $bank_id = null;
@@ -546,15 +546,15 @@ class InconsistenceController extends Controller
                 if ($subData['rate'] == $itemData['rate'] && Carbon::parse($item->created_at)->diffInHours($sub->created_at) <= 24 && $subData['bank_id'] == $bank_id && $store == $itemData['store_id']) {
                     return true;
                 }
-
+ */
                 return false;
             });
 
             /*Group the filtered collection by report id */
-           /*  $filtered = $filtered->groupBy('report_id');
+            $filtered = $filtered->groupBy('report_id');
 
             $filtered = $filtered->filter(function ($item) use ($sub, $amount, $transferences_quantity) {
-                $amountLocal = 0;
+                /* $amountLocal = 0;
                 $transferences_quantityLocal = 0;
                 $subData = json_decode($sub->data, true);
                 foreach ($item as $subreport) {
@@ -564,14 +564,14 @@ class InconsistenceController extends Controller
                 }
                 if ($amountLocal == $amount && $transferences_quantityLocal == $transferences_quantity) {
                     return true;
-                }
+                } */
 
                 return false;
             });
 
             $filtered = $filtered->flatten();
 
-            return $this->check_if_have_matches($filtered, $sub); */
+            return $this->check_if_have_matches($filtered, $sub);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
